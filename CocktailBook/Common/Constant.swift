c4b4a7864f9a1bee28262c5e6997392a2bfefe90
thukaram <sriram.2202@gmail.com>
2024-03-24 15:24:26 +0530

    Created a folder structure Created enum for type of cocktails (CocktailType)

---
+++ CocktailBook/Common/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  CocktailBook
+//
+//  Created by apple on 3/24/24.
+//
+
+import Foundation
+
+enum CocktailType: String {
+    case all = "All"
+    case alcoholic = "Alcoholic"
+    case nonAlcoholic = "Non-Alcoholic"
+}
